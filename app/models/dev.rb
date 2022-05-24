d737011c953ff10219c3610e1de435fdd3269371
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-24 11:24:53 -0400

    commit tree acb8089068d24d8d892654c35705967c208cee18

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        if self.freebies.find_by(item_name: item_name)
+            true
+        else false
+        end
+    end
+
+    def give_away(dev, freebie)
+        self.freebies.find_by(item_name: freebie.item_name).update(dev_id: dev.id)
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,11 @@ puts "Creating freebies..."
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+# Create freebies  Here
+
+Freebie.create(item_name: "Lah Lah", value: 20, company_id: 1, dev_id: 1)
+Freebie.create(item_name: "Young", value: 2, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "Testing", value: 9, company_id: 2, dev_id: 4)
+Freebie.create(item_name: "What Me", value: 50, company_id: 3, dev_id: 2)
 
 puts "Seeding done!"
